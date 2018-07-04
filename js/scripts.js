72397b643770421b2b668d4344fb5c9426ab9b39
rajan jha <rajahit16@gmail.com>
2018-07-04 15:26:53 +0530

    event listener on trophy

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,30 @@
-let bars;
+let bars,trophy;
 bars = document.querySelector(".bars");
-/* click on bars open user section at screen*/
-function userOpen(e){
+trophy = document.querySelector(".trophy");
+
+/* bars click listener*/
+function toggleUser(e){
 	let user = document.querySelector(".user");
+  let scorer = document.querySelector(".top-scorer");
+  //first toggle top-scorer
+  if(scorer.classList.contains("scorer-open-close")){
+    scorer.classList.toggle("scorer-open-close");
+    }
 	user.classList.toggle("user-open-close") ;
 }
 
-/*click listener on bars*/
-bars.addEventListener("click",userOpen);
+/* trophy click listener*/
+function toggleScorer(e) {
+  let scorer = document.querySelector(".top-scorer");
+  let user = document.querySelector(".user");
+  //first toggle toggle toggle user
+  if(user.classList.contains("user-open-close")){
+    user.classList.toggle("user-open-close");
+  }
+  scorer.classList.toggle("scorer-open-close");
+}
+
+/* register click event on bars*/
+bars.addEventListener("click",toggleUser);
+/*register click event on trophy*/
+trophy.addEventListener("click",toggleScorer);
