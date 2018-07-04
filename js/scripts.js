bc6163ff36837ca7a239dd78f2f19a5f2952596d
rajan jha <rajahit16@gmail.com>
2018-07-04 10:13:35 +0530

    remove f-screen click listener on game

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-let game = document.querySelector("section.game");
-game.addEventListener("click", function(e){
-  console.log("inside");
-document.querySelector(".page-header").classList.toggle("full-screen");
-document.querySelector(".user").classList.toggle("full-screen");
-document.querySelector(".top-scorer").classList.toggle("full-screen");
-document.querySelector("footer").classList.toggle("full-screen");
-document.querySelector(".card").classList.toggle("f-g-m");
-});
