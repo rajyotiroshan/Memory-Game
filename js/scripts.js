fe0c3860c142048c7004741883877a8222fdaf1d
rajan jha <rajahit16@gmail.com>
2018-07-05 16:37:47 +0530

    listener on all card just to flip

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-let bars,trophy;
+let bars,trophy,cards;
+let cardClickedCounter = 0;//no of card currently flipped
 bars = document.querySelector(".bars");
 trophy = document.querySelector(".trophy");
+cards = document.querySelectorAll(".card");
 
 /* bars click listener*/
 function toggleUser(e){
@@ -24,7 +26,22 @@ function toggleScorer(e) {
   scorer.classList.toggle("scorer-open-close");
 }
 
+/*click listener on card*/
+function cardClickedListener(e) {
+	e.target.classList.toggle("is-flipped");
+}
+
+function addClickEvent(card){
+	card.addEventListener("click",function(e){
+		card.classList.toggle("is-flipped");
+	});
+}
+
 /* register click event on bars*/
 bars.addEventListener("click",toggleUser);
 /*register click event on trophy*/
 trophy.addEventListener("click",toggleScorer);
+/* register click listener on card*/
+for(let i=0; i<cards.length; i++){
+	addClickEvent(cards[i]);
+}
