8d3ec3332c58ce59194b6a3f4f561507d33cd82f
rajan jha <rajahit16@gmail.com>
2018-07-08 19:01:39 +0530

    implemented game rule on card-flipping

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-let bars,trophy,cards;
+let bars, trophy, cards;
+let current_flipped_card=null, previous_flipped_card=null;
 let cardClickedCounter = 0;//no of card currently flipped
 bars = document.querySelector(".bars");
 trophy = document.querySelector(".trophy");
@@ -6,9 +7,9 @@ cards = document.querySelectorAll(".card");
 
 /* generate random integer from 1 and 16 */
 function generateRandomNo(){
-	let n = Math.random();// between 0 and 1_
+	let n = Math.random();// between 0 and 1
 	n = 8 * n;// 0 and 16
-	n = Math.ceil(n);//integer bw 0 16
+	n = Math.ceil(n);//integer bw 8
 	return n;
 }
 /* randomly assign no from 1-16 to the div.back-face*/
@@ -42,15 +43,33 @@ function toggleScorer(e) {
   scorer.classList.toggle("scorer-open-close");
 }
 
-/*click listener on card*/
-function cardClickedListener(e) {
-	e.target.classList.toggle("is-flipped");
+/*remove click listener on card*/
+function removeHandler(card){
+	card.removeEventListener("click", cardClickedListener);
 }
 
-function addClickEvent(card){
-	card.addEventListener("click",function(e){
-		card.classList.toggle("is-flipped");
-	});
+/*click listener on card*/
+function cardClickedListener(e) {
+	if(current_flipped_card === null){
+		current_flipped_card = e.currentTarget;
+		current_flipped_card.classList.toggle("is-flipped");
+		return;
+	}
+	else if (previous_flipped_card === null) {
+		previous_flipped_card = current_flipped_card;
+		current_flipped_card = e.currentTarget;
+		current_flipped_card.classList.toggle("is-flipped");
+	}
+	if(previous_flipped_card != null && current_flipped_card.lastElementChild.textContent !== previous_flipped_card.lastElementChild.textContent){
+		current_flipped_card.classList.toggle("is-flipped");
+		previous_flipped_card.classList.toggle("is-flipped");
+	}
+	else {
+		removeHandler(current_flipped_card);
+		removeHandler(previous_flipped_card);
+	}
+		current_flipped_card = null;
+		previous_flipped_card = null;
 }
 
 /* register click event on bars*/
@@ -59,7 +78,7 @@ bars.addEventListener("click",toggleUser);
 trophy.addEventListener("click",toggleScorer);
 /* register click listener on card*/
 for(let i=0; i<cards.length; i++){
-	addClickEvent(cards[i]);
+	cards[i].addEventListener("click",cardClickedListener);
 }
 /* assign no*/
 assignValueToBackFace();
